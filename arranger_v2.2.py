3da6cdfa7e5ee29af16024e7d1b86a8256f9a6b6
Thang Vo <thanhthang3375@gmail.com>
2023-08-16 11:21:26 +0700

    Add funtion updateDB()

--- arranger_v2.2.py
+++ arranger_v2.2.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from tkinter import *
 from tkinter import filedialog
 import datetime
+import mysql.connector
 
 today = datetime.datetime.now()
 folderName = str(today.month) + "." + str(today.day)
@@ -27,14 +28,36 @@ colorList = [
 ]
 
 data = [
-    ["3XL", "2XL", "XL", "L", "M", "S"],
+    ["3XL", "2XL", "XL", "L", "M"],
+    ["3XL LARGE", "2XL LARGE", "XL LARGE", "LARGE", "MEDIUM", "SET"],
+    ["3XL LARGE FB","2XL LARGE FB","XL LARGE FB","LARGE FB","MEDIUM FB","SET FB",]
+]
+
+dataFolder = [
     ["3XL LARGE", "2XL LARGE", "XL LARGE", "LARGE", "MEDIUM", "SET", "SMALL"],
     ["3XL LARGE FB","2XL LARGE FB","XL LARGE FB","LARGE FB","MEDIUM FB","SET FB",]
 ]
 
 typeList = ["MUGS", "DON UU TIEN", "FIX ISSUES"]
 
+STATUS = "UPLOADED"
 
+
+def updateDB(status, orderCode):
+    conn = mysql.connector.connect(
+    host = "seller.flashship.net", 
+    user = "flashship", 
+    password = "sjvnZVc6cwqUzk", 
+    database = "fplatform",
+    auth_plugin='mysql_native_password'
+    )
+    connObj = conn.cursor()
+    querySmall = "UPDATE orders SET status = %s where order_code = %s"
+    val = (status, orderCode)
+    connObj.execute(querySmall, val)
+    conn.commit()
+    conn.close()
+    
 # Choose file path of folder that contains Input và Output (need create folder Input before running tool; folder Output will be automatically created)
 def getNewPathInput(pathInput, pathOutput):
     print("SELECT FOLDER THAT CONTAINS INPUT: ")
@@ -48,7 +71,6 @@ def getNewPathInput(pathInput, pathOutput):
     print("  OUTPUT DIRECTORY: ", pathOutput)
     print("- - - - - - - - - - - - - - - - - - -")
 
-
 # Create document directory by Sizes và Colors, folder Output will be automatically created
 def createTemplate():
     print("- - - - - - - - - - - - - - - - - - -")
@@ -56,10 +78,10 @@ def createTemplate():
     for typer in typeList:  # create folder by typeList
         os.makedirs(pathOutput + typer)
         print(pathOutput + typer + " ... CREATED!")
-    for size in data[1]:
+    for size in dataFolder[0]:
         os.makedirs(pathNewOrder + size)  # create folder by sizeList
         print(pathNewOrder + size + " ... CREATED!")
-    for sizefb in data[2]:
+    for sizefb in dataFolder[1]:
         os.makedirs(pathNewOrder + sizefb)  # create folder by sizeListFB
         print(pathNewOrder + sizefb + " ... CREATED!")
     os.makedirs(pathColor)  # create folder by colorList
@@ -68,39 +90,6 @@ def createTemplate():
         print(pathColor + color + " ... CREATED!")
     print("TEMPLATES CREATED.")
     print("- - - - - - - - - - - - - - - - - - -")
-
-
-# Count shirts
-def countShirt():
-    os.chdir(pathInput)
-    countSize = 0  # for counting files
-    countFB = 0 # for counting files FB
-    shirts = 0 # for counting shirts
-    shirtsFB = 0 # for counting shirts have FB
-    for file in os.listdir():
-        name, size = os.path.splitext(file)
-        splitByUnderline = name.split("_")  # get data that was splitted by "_"
-        splitted = [
-            s.strip() for s in splitByUnderline
-        ]  # xóa khoảng trắng 2 đầu string
-        splitBySize = splitted[2].split("-")  # get data that was splitted by "-"
-        if splitted[5] != "1-1":  # Arrange files by SET and SET FB
-            if splitted[1] == "FB":
-                countFB += 1
-            else:
-                countSize += 1
-        else:
-            for i in range(6):
-                if splitBySize[0] == data[0][i]:
-                    if splitted[1] == "FB":
-                        countFB += 1
-                    else:
-                        countSize += 1
-    shirtsFB = countFB / 2
-    shirts = countSize
-    print(" ", shirtsFB, "SHIRTS-FB DONE.")
-    print(" ", shirts, "SHIRTS DONE.")
-
 
 # Arrange files by Colors: RED, NAVY, ROYALBLUE, ... in colorList[]
 def arrangeFilesByColor():
@@ -111,11 +100,28 @@ def arrangeFilesByColor():
         name, size = os.path.splitext(file)
         splitByUnderline = name.split("_")  # get data that was splitted by "_"
         splitted = [s.strip() for s in splitByUnderline]
+        orderCode = splitted[4]
+        orderCodeS = splitted[3]
         splitByColor = splitted[2].split("-")  # get data that was splitted by "-"
-        for color in colorList:
-            if splitByColor[1] == color:
-                shutil.move(file, pathColor + color)
+        if splitByColor[0] == "S":
+            shutil.move(file, pathNewOrder + "SMALL")
+            countColor += 1
+            updateDB(STATUS,orderCodeS)
+        elif splitted[5] != "1-1":  # Arrange files by SET and SET FB
+            if splitted[1] == "FB":
+                shutil.move(file, pathNewOrder + "SET FB")
                 countColor += 1
+                updateDB(STATUS,orderCode)
+            else:
+                shutil.move(file, pathNewOrder + "SET")
+                countColor += 1
+                updateDB(STATUS,orderCode)
+        else:
+            for color in colorList:
+                if splitByColor[1] == color:
+                    shutil.move(file, pathColor + color)
+                    countColor += 1
+                    updateDB(STATUS,orderCode)
         inputQuantity += 1
     print(" ", inputQuantity, "FILES IN INPUT:")
     print(" ", countColor, "FILES BY COLOR DONE.")
@@ -125,44 +131,26 @@ def arrangeFilesByColor():
 def arrangeFilesBySize():
     os.chdir(pathInput)
     countSize = 0  # for counting files
-    countFB = 0 # for counting files FB
-    shirts = 0 # for counting shirts
-    shirtsFB = 0 # for counting shirts have FB
     for file in os.listdir():
         name, size = os.path.splitext(file)
         splitByUnderline = name.split("_")  # get data that was splitted by "_"
         splitted = [
             s.strip() for s in splitByUnderline
         ]  # xóa khoảng trắng 2 đầu string
+        orderCode = splitted[4]
         splitBySize = splitted[2].split("-")  # get data that was splitted by "-"
-        if splitted[5] != "1-1":  # Arrange files by SET and SET FB
-            if splitted[1] == "FB":
-                shutil.move(file, pathNewOrder + "SET FB")
-                countFB += 1
-            else:
-                shutil.move(file, pathNewOrder + "SET")
-                countSize += 1
-        else:
-            for i in range(6):
-                if splitBySize[0] == data[0][i]:
-                    if splitted[1] == "FB":
-                        shutil.move(file, pathNewOrder + data[2][i])
-                        countFB += 1
-                    else:
-                        shutil.move(file, pathNewOrder + data[1][i])
-                        countSize += 1
-    print(" ", countFB+countSize, "FILES DONE")
+        for i in range(5):
+            if splitBySize[0] == data[0][i]:
+                if splitted[1] == "FB":
+                    shutil.move(file, pathNewOrder + data[2][i])
+                    countSize += 1
+                    updateDB(STATUS,orderCode)
+                else:
+                    shutil.move(file, pathNewOrder + data[1][i])
+                    countSize += 1
+                    updateDB(STATUS,orderCode)
+    print(" ", countSize, "FILES DONE")
 
-# def countSmall():
-#     pathSmall = pathNewOrder + "/SMALL"
-#     os.chdir(pathSmall)
-#     for file in os.listdir():
-#         name, size = os.path.splitext(file)
-#         splitByUnderline = name.split("_")  # get data that was splitted by "_"
-#         splitted = [
-#             s.strip() for s in splitByUnderline
-#         ] 
-#         print(len(splitByUnderline))
 
 def main():
     # key1 = input(
@@ -177,20 +165,16 @@ def main():
         createTemplate()
         print("VICTOR'S TOOL IS WORKING...")
         print("- - - - - - - - - - - - - - - - - - -")
-        countShirt()
         arrangeFilesByColor()
         arrangeFilesBySize()
-        # countSmall()
         os.system("pause")
     elif key2 == "1":
         print("VICTOR'S TOOL IS WORKING...")
         print("- - - - - - - - - - - - - - - - - - -")
-        countShirt()
         arrangeFilesByColor()
         arrangeFilesBySize()
-        # countSmall()
         os.system("pause")
     else:
         exit
-
+    
 main()
