43c0c4c08c14f8df52dc6c8c6f404faf792654ac
Thang Vo <thanhthang3375@gmail.com>
2023-11-02 13:48:47 +0700

    Add Black Formatting

--- flashpod_v2.py
+++ flashpod_v2.py
@@ -10,40 +10,41 @@ folder_root = "THANG" + str(today.month)
 path_input = "E:/US/PDFFILES/Input"
 path_output = "E:/Dropbox/THANG 11/" + folder_name + "/"
 
-""" 
-    SELLER_LIST = ["DON MOI", "MERCHFOX"]
-    COLOR = ["BLACK", "WHITE", "COLORS"]
-    SIZE = ["SMALL", "MEDIUM", "LARGE", "XL LARGE", "2XL LARGE", "3XL LARGE"]
-    POSITION = "FB"
-    TYPE = ["SHIRT", "HOODIE", "SWEATSHIRT"]
-    FOLDER_EXTRA = ["DON GUI LAI", "DON UU TIEN", "FIX ISSUES"]
-    SELF_LABEL = ["MERCHFOXSUPPORT", "MBTEAM", "NGUYENTHUY", "SUPPORTTUAN", "PHAMPHONGPHU", "TDATEAMSUPPORT"]
-""" 
+# SELLER_LIST = ["DON MOI", "MERCHFOX"]
+# COLOR = ["BLACK", "WHITE", "COLORS"]
+# SIZE = ["SMALL", "MEDIUM", "LARGE", "XL LARGE", "2XL LARGE", "3XL LARGE"]
+# POSITION = "FB"
+# TYPE = ["SHIRT", "HOODIE", "SWEATSHIRT"]
+# FOLDER_EXTRA = ["DON GUI LAI", "DON UU TIEN", "FIX ISSUES"]
+# SELF_LABEL = ["MERCHFOXSUPPORT", "MBTEAM", "NGUYENTHUY", "SUPPORTTUAN", "PHAMPHONGPHU", "TDATEAMSUPPORT"]
 
-SELF_LABEL = ["MERCHFOXSUPPORT", "MBTEAM", "NGUYENTHUY", "SUPPORTTUAN", "PHAMPHONGPHU", "TDATEAMSUPPORT"]
+SELF_LABEL = [
+    "MERCHFOXSUPPORT",
+    "MBTEAM",
+    "NGUYENTHUY",
+    "SUPPORTTUAN",
+    "PHAMPHONGPHU",
+    "TDATEAMSUPPORT",
+]
+
 
 def splitted_by_underline(file):
-    """Thay thế dấu "-" thành dấu "_" để đồng nhất
-    Tên file là String chứa các keyword :
-    Tách các keyword phân cách bởi "_" thêm vào 1 list
+    """Tên file là dạng String gồm những keyword thể hiện các giá trị : date, seller, size, ... của product
+            phân tách bởi dấu "-" hoặc "_"
+            Để tách keyword cho tùy mục đích sử dụng (chia file, thống kê)
+    Args:
+        file
+
+    Actions:
+        Thay thế dấu "-" thành dấu "_" để đồng nhất
+        Tách các keyword phân cách bởi "_"
+        Xóa khoảng trắng đầu cuối
+        Thêm keyword vào list splitted
+
+    Return:
+        List splitted chứa các keyword
     """
     name, _ = os.path.splitext(file)
-    name = name.replace("-", "_")
-    split_by_underline = name.split("_")
-    splitted = [s.strip() for s in split_by_underline]
-    return splitted
-
-
-def splitted_by_extension(file):
-    """get word that splited by .pdf extension
-
-    Args:
-        file (_.pdf_): file with .pdf extension
-
-    Returns:
-        list: list of words
-    """
-    name, _ = file.split(".pdf")
     name = name.replace("-", "_")
     split_by_underline = name.split("_")
     splitted = [s.strip() for s in split_by_underline]
@@ -100,15 +101,20 @@ def create_path(Order):
     order = Order
     for seller in SELF_LABEL:
         if order.seller == seller:
-            path = os.path.join(path_output, "KHACH TU MUA LABEL", order.size + " " + order.side)
+            path = os.path.join(
+                path_output, "KHACH TU MUA LABEL", order.size + " " + order.side
+            )
             return path
         elif order.seller == "MERCHFOX":
             if order.set != "1":
                 path = os.path.join(path_output, order.seller, "SET " + order.side)
             elif order.side == "FB":
                 path = os.path.join(
-                    path_output, order.seller, order.color, order.size + " " + order.side
-                    )
+                    path_output,
+                    order.seller,
+                    order.color,
+                    order.size + " " + order.side,
+                )
             else:
                 path = os.path.join(path_output, order.seller, order.color, order.size)
         else:
@@ -123,7 +129,7 @@ def create_path(Order):
     return path
 
 
-def main():
+def core():
     os.chdir(path_input)
     for file in os.listdir():
         order = create_order(file)
@@ -131,7 +137,6 @@ def main():
         try:
             if os.path.exists(path):
                 shutil.move(file, path)
-                # print(path_output)
                 print("Completed")
             else:
                 os.makedirs(path)
@@ -139,6 +144,10 @@ def main():
                 print("Completed")
         except IndexError as err:
             print("Error: ", err)
+
+
+def main():
+    core()
 
 
 main()
